12b2d6e94d78fa105229324a69f76b69c2071055
NATHNAEL GEBREGZIABHER WELDEGIORGIS <86237147+Nathy2021@users.noreply.github.com>
2022-12-26 23:26:30 -0600

    added code

--- src/gitest/GitTest.java
+++ src/gitest/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Github");
 		System.out.println("Hello Meron");
+		System.out.println("Hello Meron2");
 
 	}
 
